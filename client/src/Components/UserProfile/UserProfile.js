6be13db130b7c0d81136ed66364daffd91dbba0c
Hitesh Sankhat <hitesh.sankhat@zeuslearning.com>
2021-07-10 20:22:36 +0530

    user profile header section

--- client/src/Components/UserProfile/UserProfile.js
+++ client/src/Components/UserProfile/UserProfile.js
@@ -1,33 +1,65 @@
-import React from 'react'
-import * as style from './UserProfile.styles'
+import React from "react";
+import * as style from "./UserProfile.styles";
+import localization from "../localization.json";
 
-function UserProfile() {
+function UserProfile({ user }) {
+  const userProfileLocalization = localization.UserProfileComponent;
+  console.log(user);
   return (
     <div>
       <style.ProfileContent>
         <style.HeaderContent>
           <style.ProfileImageContainer>
-            <style.ProfileImage alt="test" src="https://source.unsplash.com/user/erondu/150x150">
-            </style.ProfileImage>
+            <style.ProfileImage
+              alt="test"
+              src={
+                user && user.ProfileImage
+                  ? user.ProfileImage
+                  : "https://source.unsplash.com/user/erondu/150x150"
+              }
+            ></style.ProfileImage>
           </style.ProfileImageContainer>
-          <style.UserDataContianer>
+          <style.UserDataContainer>
             <style.UserNameSection>
-              <h2>Hiteshsankhat</h2>
-              <button> Edit Profile</button>
+              <style.UserNameSectionH2>
+                {user && user.UserName ? user.UserName : ""}
+              </style.UserNameSectionH2>
+              <style.UserNameSectionButton>
+                {userProfileLocalization.editProfile}
+              </style.UserNameSectionButton>
             </style.UserNameSection>
             <style.UserFollowerSection>
-              <p>post</p>
-              <p>post</p>
-              <p>post</p>
+              <style.UserFollowerSectionText>
+                <style.UserFollowerSectionNumber>
+                  7
+                </style.UserFollowerSectionNumber>
+                {userProfileLocalization.posts}
+              </style.UserFollowerSectionText>
+              <style.UserFollowerSectionText>
+                <style.UserFollowerSectionNumber>
+                  {user && user.Followers && user.Followers.length
+                    ? user.Followers.length
+                    : 0}
+                </style.UserFollowerSectionNumber>
+                {userProfileLocalization.follower}
+              </style.UserFollowerSectionText>
+              <style.UserFollowerSectionText>
+                <style.UserFollowerSectionNumber>
+                  {user && user.Following && user.Following.length
+                    ? user.Following.length
+                    : 0}
+                </style.UserFollowerSectionNumber>
+                {userProfileLocalization.following}
+              </style.UserFollowerSectionText>
             </style.UserFollowerSection>
             <style.UserBioSection>
-              <p>Bio</p>
+              <p>{user && user.Bio ? user.Bio : "Bio"}</p>
             </style.UserBioSection>
-          </style.UserDataContianer>
+          </style.UserDataContainer>
         </style.HeaderContent>
       </style.ProfileContent>
     </div>
-  )
+  );
 }
 
-export default UserProfile
+export default UserProfile;
