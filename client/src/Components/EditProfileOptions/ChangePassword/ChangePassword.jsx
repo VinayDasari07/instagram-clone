10e781390a08009b1aebd94546e4498787a84311
Hitesh Sankhat <hitesh.sankhat@zeuslearning.com>
2021-08-15 21:05:22 +0530

    change password input value match

--- client/src/Components/EditProfileOptions/ChangePassword/ChangePassword.jsx
+++ client/src/Components/EditProfileOptions/ChangePassword/ChangePassword.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChangePassword.css";
 
 export const ChangePassword = () => {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const changePasswordClick = (e) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      alert("Password does not match");
+    }
+    const changePasswordObj = {
+      oldPassword,
+      newPassword,
+    };
+    console.log(changePasswordObj);
+  };
+
   return (
     <div className="change-password">
       <div className="old-password">
         <label className="label" htmlFor="old-password">
           Old Password
         </label>
-        <input type="password" name="old-password" id="old-password" />
+        <input
+          type="password"
+          name="old-password"
+          id="old-password"
+          onChange={(e) => setOldPassword(e.target.value)}
+        />
       </div>
       <div className="new-password">
         <label className="label" htmlFor="new-password">
           New Password
         </label>
-        <input type="password" name="new-password" id="new-password" />
+        <input
+          type="password"
+          name="new-password"
+          id="new-password"
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
       </div>
       <div className="confirm-new-password">
         <label className="label" htmlFor="confirm-new-password">
@@ -24,10 +50,15 @@ export const ChangePassword = () => {
           type="password"
           name="confirm-new-password"
           id="confirm-new-password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />
       </div>
       <div className="btn-center">
-        <button className="submit-btn" type="submit">
+        <button
+          className="submit-btn"
+          type="submit"
+          onClick={(e) => changePasswordClick(e)}
+        >
           Change Password
         </button>
       </div>
